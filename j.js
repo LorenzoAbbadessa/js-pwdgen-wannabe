5d2d68385bbf108acaa5bf09191c2619a8a45a5b
lorenzo <lor.abbadessa@gmail.com>
2023-10-25 16:05:54 +0200

    finisce esercizio

---
+++ j.js
@@ -0,0 +1,6 @@
+
+const nomeUtente = prompt( "Inserisci il tuo nome", "LOR");
+const cognomeUtente = prompt("Inserisci la tua età", "ABBA");
+const favColor = prompt("Inserisci la tua età", "black");
+
+document.getElementById("titolo").innerHTML = nomeUtente + cognomeUtente + favColor + "23#";
